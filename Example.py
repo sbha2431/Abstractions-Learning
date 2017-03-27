b0ff42ad1a73c0573470418e034fbfc796cbf13a
Suda Bharadwaj <suda.b@utexas.edu>
2017-03-27 14:05:36 -0500

    Example added

--- Example.py
+++ Example.py
@@ -1,4 +1,5 @@
 from mdp import MDP
+import abstraction
 from itertools import product
 """Example problem"""
 ''
@@ -29,7 +30,7 @@ transitions.add((3,'b',2,0.99))
 transitions.add((4,'a',4,1))
 transitions.add((4,'b',4,1))
 
-mdp = MDP(states,set(),acts,transitions)
+mdp = MDP(states,acts,transitions)
 
 #Define rewards
 Rs = dict((s,0) for s in states)
@@ -39,14 +40,18 @@ Rs[2] = 3
 Rs[3] = 10
 Rs[4] = 0
 Rs[5] = -1
-R = dict(((s,a,t),0) for (s,a,t) in mdp.transitions)
+R = dict(((s,a),0) for (s,a,t) in mdp.transitions)
 
 for s in mdp.states:
     for a in mdp.available(s):
         for t in mdp.post(s,a):
-            R[(s,a,t)] = Rs[t]
+            R[(s,a)] = Rs[t]
 
-print R
-U,p = mdp.T_step_value_iteration(R,50)
-print U
-print p
+# abstraction
+aggregation = dict()
+aggregation[0] = {0}
+aggregation[1] = {1,2,3}
+aggregation[2] = {4,5}
+print aggregation
+absmdp = abstraction.create_abstractMDP(mdp,aggregation)
+print absmdp
