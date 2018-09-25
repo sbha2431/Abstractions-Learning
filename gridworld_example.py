6cdbcfc8c4b5621bdbc5d8443a4248865d59bc54
sbha2431 <suda.b@utexas.edu>
2018-09-25 15:09:30 -0500

    active classification

--- gridworld_example.py
+++ gridworld_example.py
@@ -4,6 +4,10 @@ import numpy as np
 from mdp import MDP
 from tqdm import tqdm
 
+def get_indices_of_k_smallest(arr, k):
+    idx = np.argpartition(arr.ravel(), k)
+    return tuple(np.array(np.unravel_index(idx, arr.shape))[:, range(min(k, 0), max(k, 0))])
+
 #gridworld example
 
 nrows = 10
@@ -36,14 +40,19 @@ V, policyT = mdp.max_reach_prob(set(targets[0]), epsilon=0.0001)
 V, policyT1 = mdp.max_reach_prob(set([80,81,90,91]), epsilon=0.0001)
 agentbehaviours = [policyT,policyT1]
 transitions = []
+randomness = 0
 for ab in agentbehaviours:
     for s in states:
         transdict = dict([(s, agentbehaviours.index(ab), t), 0.0] for t in states)
-        for a in ab[s]:
+        for a in range(gwg.nactions):
+            if a in ab[s]:
+                w = 1.0/len(ab[s]) - randomness/(gwg.nstates - len(ab[s]))
+            else:
+                w = (1.0 / (gwg.nstates - len(ab[s])))*randomness
             # tempdict = dict([(s, a, t),0.0] for t in states)
             for t in np.nonzero(gwg.prob[gwg.actlist[a]][s])[0]:
                 p = gwg.prob[gwg.actlist[a]][s][t]
-                transdict[(s, agentbehaviours.index(ab), t)] += p*1.0/len(ab[s])
+                transdict[(s, agentbehaviours.index(ab), t)] += p*w
         for t in states:
             transitions.append((s, agentbehaviours.index(ab), t, transdict[(s, agentbehaviours.index(ab), t)]))
 mdp1 = MDP(states,alphabet=range(2),transitions=transitions)
@@ -73,7 +82,7 @@ mdp2.write_to_file('Class2.txt',initial[0],targets=set(targets[0]))
 transitions = []
 alphabet = {0,1}
 accepting_states = set()
-file = open('beliefMDP.txt', 'r')
+file = open('beliefMDP_small.txt', 'r')
 print 'Reading MDP'
 for line in tqdm(file):
     l = line.split()
@@ -103,16 +112,16 @@ R.update([(s,a,next_s),1.0] for s in mdp.states  for a in mdp.available(s) for n
 # print R
 # Solving belief
 print('Solving belief MDP')
-V,P =  mdp.T_step_value_iteration(R,20)
+V,P =  mdp.T_step_value_iteration(R,10)
 # print P
 # print V
 
 print 'Simulating Belief'
 beliefMap = dict()
-file = open('beliefStateMapping.txt', 'r')
+file = open('beliefStateMapping_small.txt', 'r')
 for line in tqdm(file):
     l = line.split(':')
-    if '(s b c)' not in l[0]:
+    if '(s,b,c)' not in l[0]:
         ns = int(l[1])
         l = l[0].split(',')
         s = int(l[0])
@@ -121,28 +130,50 @@ for line in tqdm(file):
         l = l[1].split(']')[0]
         l = l.split(' ')
         b = map(float,l)
+        b[0] = round(b[0],6)
+        b[1] = round(1 - b[0],6)
         beliefMap[(s,tuple(b),c)] = ns
 # print beliefMap
 
 curr_s = initial[0]
 c = 0
 curr_b = (0.5,0.5)
-groundmdp = mdp2
+groundmdp = mdp1
 allmdps = [mdp1,mdp2]
 cost_dict = {0:1,1:5}
+allbs = np.array([beliefMap.keys()[i][1] for i in range(len(beliefMap))])
+indkey = 0
+curr_b_s = 0
 while True:
-    curr_b_s = beliefMap[(curr_s,curr_b,c)]
     if curr_b_s in accepting_states:
         break
     a = P[curr_b_s].pop()
+    print 'Taking action ', a
     next_s = groundmdp.sample(curr_s,a)
+    print 'next state ', next_s
     gwg.current = [next_s]
     gwg.render()
+    raw_input("Press Enter to continue...")
+    print 'Computing new belief'
     c+=cost_dict[a]
     b = [0,0]
     for i in range(2):
         b[i] = allmdps[i].prob_delta(curr_s,a,next_s)*curr_b[i]/(sum([allmdps[j].prob_delta(curr_s,a,next_s)*curr_b[j] for j in range(2) ]))
+    b[0] = round(b[0], 6)
+    b[1] = round(1 - b[0], 6)
     curr_b = tuple(b)
+    bs = np.full((len(beliefMap),2),curr_b)
+    k=1000
+    indkeyset = get_indices_of_k_smallest(np.linalg.norm(allbs - bs, axis=1),k)[0]
+    ind  = 0
+    indkey = indkeyset[ind]
+    while beliefMap.keys()[indkey] != (curr_s,beliefMap.keys()[indkey][1],c):
+        ind+=1
+        if ind == k:
+            print 'stff'
+            break
+        indkey = indkeyset[ind]
     print curr_b
+    curr_b_s = beliefMap[beliefMap.keys()[indkey]]
 
 print curr_b
